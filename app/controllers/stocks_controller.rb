128c72397cbadeaa48826f6b9045adbc8d801f21
Adam <Adamiss@users.noreply.github.com>
2019-01-21 16:10:18 +0100

    Search for a stock and display it

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,18 @@
 class StocksController < ApplicationController
 
   def search
+    if params[:stock].present?
+      @stock =  Stock.new_from_lookup(params[:stock])
+      if @stock
+        render "users/my_portofolio "
+      else
+        flash[:danger] = "Yu have entered an incorrect symbol"
+        redirect_to my_portofolio_path
+      end
+    else
+      flash[:danger] = "You have entered an empty string"
+      redirect_to my_portofolio_path
+    end
 
   end
 end
